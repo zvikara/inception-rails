ffee379e700ac14e03a227517dd66872e9b02ddd
Zvika Rap <zvikara@gmail.com>
2012-11-09 15:34:22 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InceptionRails::Application.config.secret_token = 'ea1b5c2a90151250fe0e72e873906b866ba82515426cb8434309f2840421a287d4ca7d92aa05f405d91bd0a66f8fec659984a03d4db6a166db6358a9a37adf3e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+InceptionRails::Application.config.session_store :cookie_store, key: '_inception-rails_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# InceptionRails::Application.config.session_store :active_record_store

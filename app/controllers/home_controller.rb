6baeb7c55f6ca1e4643546d1c7b0067585b0ca26
Zvika Rap <zvikara@gmail.com>
2012-11-09 22:07:30 +0200

    css and web content

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -14,5 +14,6 @@ class HomeController < ApplicationController
     #@albums = @graph.get_connections("me", "albums")
     #@photos = @graph.get_connections("me", "photos")
     @photos = @graph.fql_query("SELECT src_big FROM photo WHERE owner = me() OR owner IN (SELECT uid2 FROM friend WHERE uid1 = me() LIMIT 10) LIMIT 200")
+    #@photos = @graph.fql_query("SELECT src FROM photo_src WHERE height = '180' AND photo_id = (SELECT pid FROM photo WHERE owner = me() OR owner IN (SELECT uid2 FROM friend WHERE uid1 = me() LIMIT 5) LIMIT 80)")
   end
 end

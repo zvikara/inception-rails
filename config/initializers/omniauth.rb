5c5a64d5cf67ce38f46b797987c58b2dafdd0a1e
Zvika Rap <zvikara@gmail.com>
2012-11-10 09:39:53 +0200

    facebook key

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], { 
+  provider :facebook, '21531420672', '6856c8af8442619c2c516a07fe1301d2', { 
   	:display => 'popup',
   	:scope => 'email, user_photos, friends_photos'
   }

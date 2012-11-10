628476d739893082d70500900cb2e4cb2b2e0e08
Zvika Rap <zvikara@gmail.com>
2012-11-10 10:32:28 +0200

    ca-cert4

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, '21531420672', '6856c8af8442619c2c516a07fe1301d2', {
-  	:client_options => { :ssl => { :ca_path => "/usr/lib/ssl/certs" } },
+  	#:client_options => { :ssl => { :ca_path => "/usr/lib/ssl/certs" } },
   	:display => 'popup',
   	:scope => 'email, user_photos, friends_photos'
   }

33e59b0247910d86f4a08358854ac67be2ee4983
Zvika Rap <zvikara@gmail.com>
2012-11-10 10:13:01 +0200

    ca-cert2

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, '21531420672', '6856c8af8442619c2c516a07fe1301d2', {
-  	:client_options => { :ssl => { :ca_file => "#{Rails.root}/config/ca-bundle.crt" },
+  	:client_options => { :ssl => { :ca_file => "#{Rails.root}/config/ca-bundle.crt" } },
   	:display => 'popup',
   	:scope => 'email, user_photos, friends_photos'
   }

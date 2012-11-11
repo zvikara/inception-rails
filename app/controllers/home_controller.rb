6b81e74380c707cc51f467fdc49e81d6d8a8f924
Zvika Rap <zvikara@gmail.com>
2012-11-11 16:08:29 +0000

    changed thumbnail images to use facebook small images

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,6 +6,6 @@ class HomeController < ApplicationController
   def photos
     @access_token = current_user.authentications.first.token
     @graph = Koala::Facebook::API.new(@access_token)
-    @photos = @graph.fql_query("SELECT src_big,caption FROM photo WHERE owner = me() OR owner IN (SELECT uid2 FROM friend WHERE uid1 = me() LIMIT 8) LIMIT 100")
+    @photos = @graph.fql_query("SELECT src,src_big,caption FROM photo WHERE owner = me() OR owner IN (SELECT uid2 FROM friend WHERE uid1 = me() LIMIT 8) LIMIT 100")
   end
 end

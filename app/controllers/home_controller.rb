1fda008ca638c5ca37a7ba27a1446eccf9226c76
Zvika Rap <zvikara@gmail.com>
2012-11-11 14:13:38 +0000

    bootstrap gallery

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,14 +6,6 @@ class HomeController < ApplicationController
   def photos
     @access_token = current_user.authentications.first.token
     @graph = Koala::Facebook::API.new(@access_token)
-    #@feed = nil # @graph.get_connections('me', 'feed').first
-    #@friends = nil # @graph.get_connections("me", "friends")
-    # friends.each do |f|
-    #   f[:image]= @graph.get_picture(f["id"])
-    # end
-    #@albums = @graph.get_connections("me", "albums")
-    #@photos = @graph.get_connections("me", "photos")
-    @photos = @graph.fql_query("SELECT src_big FROM photo WHERE owner = me() OR owner IN (SELECT uid2 FROM friend WHERE uid1 = me() LIMIT 8) LIMIT 100")
-    #@photos = @graph.fql_query("SELECT src FROM photo_src WHERE height = '180' AND photo_id = (SELECT pid FROM photo WHERE owner = me() OR owner IN (SELECT uid2 FROM friend WHERE uid1 = me() LIMIT 5) LIMIT 80)")
+    @photos = @graph.fql_query("SELECT src_big,caption FROM photo WHERE owner = me() OR owner IN (SELECT uid2 FROM friend WHERE uid1 = me() LIMIT 8) LIMIT 100")
   end
 end
